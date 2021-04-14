87f4073905be080be0751e5a11da29d675a00833
Tu <hoangtu1392001@gmail.com>
2021-04-14 11:18:32 +0700

    [Add]_Setup Project

---
+++ WatchShopSolution.Application/Products/CreateProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WatchShopSolution.Database;
+using WatchShopSolution.Domain.Models;
+
+namespace WatchShopSolution.Application.Products
+{
+    public class CreateProduct
+    {
+        private readonly ApplicationDbContext _context;
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Create(int id,string name, string description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Description = description,
+                Name = name
+
+            });
+        }
+    }
+}
---
+++ WatchShopSolution.Database/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WatchShopSolution.Domain.Models;
+
+namespace WatchShopSolution.Database
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
